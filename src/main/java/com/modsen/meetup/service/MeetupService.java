db027d49b7966082e136d076d9197b210690698b
jskov <jskov259259@rambler.ru>
2022-10-28 12:25:10 +0300

    added MeetupService

---
+++ src/main/java/com/modsen/meetup/service/MeetupService.java
@@ -0,0 +1,10 @@
+package com.modsen.meetup.service;
+
+import com.modsen.meetup.model.Meetup;
+
+import java.util.List;
+
+public interface MeetupService {
+
+    List<Meetup> findAll();
+}
---
+++ src/test/java/com/modsen/meetup/dao/MeetupDaoImplTestIT.java
@@ -0,0 +1,14 @@
+package com.modsen.meetup.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MeetupDaoImplTestIT {
+
+    @Test
+    void testFindAll() {
+
+    }
+
+}
