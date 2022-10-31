3c6a8d8711a187e2f7bbab1f871713f92b217542
jskov <jskov259259@rambler.ru>
2022-10-31 11:45:05 +0300

    deleted profile

--- src/main/java/com/modsen/meetup/config/AppConfig.java
+++ src/main/java/com/modsen/meetup/config/AppConfig.java
@@ -4,10 +4,7 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -19,7 +16,6 @@ import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
-@Profile("prod")
 public class AppConfig {
 
     @Value("${postgresql_driver}")
